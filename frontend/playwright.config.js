57f46cf91391a4f88f93dac3aa40d4fd6b9ebe58
Yaswanth <yashmanic96@gmail.com>
2026-09-25 19:02:35 +0530

    updated packages && changed playwright conf

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: "pnpm run dev",
+    command: process.env.CI ? "pnpm run build && pnpm run preview -- --port 8080" : "pnpm run dev",
     url: "http://localhost:8080",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
